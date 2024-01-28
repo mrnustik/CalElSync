90f50e320a356bf2462150be6b57745556b3e473
Michal Mrnuštík <michal.mrnustik@outlook.com>
2024-01-28 15:24:26 +0100

    Remove not working UserSecrets from the function

--- CalElSync.Function/Program.cs
+++ CalElSync.Function/Program.cs
@@ -1,14 +1,10 @@
 using CalElSync.Core.Extensions;
 using CalElSync.Events.iCal.Extensions;
 using CalElSync.Tasks.Todoist.Extensions;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureAppConfiguration(
-        conf =>
-            conf.AddUserSecrets(typeof(Program).Assembly))
     .ConfigureServices(
         (context, services) =>
         {
