a790c5a9974b2b34fba87fce5887c3ae861a157d
Michal Mrnuštík <michal.mrnustik@outlook.com>
2024-01-28 15:36:12 +0100

    Add sync running

--- CalElSync.Function/RunSync.cs
+++ CalElSync.Function/RunSync.cs
@@ -1,33 +1,34 @@
-﻿using System.Collections.Generic;
-using System.Net;
+﻿using System.Net;
+using CalElSync.Core;
+using CalElSync.Core.Common;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Extensions.Logging;
 
 namespace CalElSync.Function;
 
 public class RunSync
 {
-    private readonly ILogger _logger;
+    private readonly ISynchronizationHandler _synchronizationHandler;
 
-    public RunSync(ILoggerFactory loggerFactory)
+    public RunSync(ISynchronizationHandler synchronizationHandler)
     {
-        _logger = loggerFactory.CreateLogger<RunSync>();
+        _synchronizationHandler = synchronizationHandler;
     }
 
     [Function("RunSync_Http")]
-    public HttpResponseData RunSync_Http(
-        [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req,
+    public async Task<HttpResponseData> RunSync_Http(
+        [HttpTrigger(AuthorizationLevel.Function, "post")]
+        HttpRequestData req,
         FunctionContext executionContext)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
-
+        await _synchronizationHandler.RunSynchronizationAsync(
+            new DateTimeInterval(
+                DateTime.Today,
+                DateTime.Today.AddDays(7)),
+            executionContext.CancellationToken);
         var response = req.CreateResponse(HttpStatusCode.OK);
         response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-        response.WriteString("Welcome to Azure Functions!");
-
+        await response.WriteStringAsync("OK!");
         return response;
-        
     }
 }
